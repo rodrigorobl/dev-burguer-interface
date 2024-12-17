7a76d63878a16b4cca0b480254e28fbad9652633
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-12-17 20:03:35 -0300

    17122024

--- src/containers/Admin/NewProducts/index.jsx
+++ src/containers/Admin/NewProducts/index.jsx
@@ -6,12 +6,23 @@ import { Image } from "@phosphor-icons/react";
 import { Container, Form, InputGroup, Label, Input, LabelUpload, Select, SubmitButton, ErrorMessage } from "./styles";
 import { useEffect, useState } from "react";
 import { api } from "../../../services/api";
+import { toast } from "react-toastify";
 
 const schema = yup.object({
-	name: yup.string().required(),
-	price: yup.number().positive().required(),
-	category: yup.object().required(),
-	file: yup.mixed(),
+	name: yup.string().required("Digite o nome do produto"),
+	price: yup.number().positive().required("Digite o preço do produto").typeError("Digite o preço do produto"),
+	category: yup.object().required("Escolha uma categoria"),
+	file: yup
+		.mixed()
+		.test("required", "Escolha um arquivo para continuar", (value) => {
+			return value && value.length > 0;
+		})
+		.test("fileSize", "Carregue arquivos até 3mb", (value) => {
+			return value && value.length > 0 && value[0].size <= 30000;
+		})
+		.test("type", "Carregue apenas imagem PNG ou JPEG", (value) => {
+			return value && value.length > 0 && (value[0].type === "image/jpeg" || value[0].type === "image/png");
+		}),
 });
 
 export function NewProducts() {
@@ -36,8 +47,19 @@ export function NewProducts() {
 	} = useForm({
 		resolver: yupResolver(schema),
 	});
-	const onSubmit = (data) => {
-		console.log(data);
+	const onSubmit = async (data) => {
+		const productFormData = new FormData();
+
+		productFormData.append("name", data.name);
+		productFormData.append("price", data.price * 100);
+		productFormData.append("category_id", data.category.id);
+		productFormData.append("file", data.file[0]);
+
+		await toast.promise(api.post("/products", productFormData), {
+			pending: "Adicionando o produto...",
+			success: "Produto adicionado com sucesso!",
+			error: "Falha ao adicionar produto, tente novamente.",
+		});
 	};
 
 	return (
@@ -70,6 +92,7 @@ export function NewProducts() {
 
 						{fileName || "Upload do Produto"}
 					</LabelUpload>
+					<ErrorMessage>{errors?.file?.message}</ErrorMessage>
 				</InputGroup>
 
 				<InputGroup>
@@ -77,7 +100,7 @@ export function NewProducts() {
 					<Controller
 						name="category"
 						control={control}
-						render={(field) => (
+						render={({ field }) => (
 							<Select
 								{...field}
 								options={categories}
@@ -88,6 +111,7 @@ export function NewProducts() {
 							/>
 						)}
 					/>
+					<ErrorMessage>{errors?.category?.message}</ErrorMessage>
 				</InputGroup>
 
 				<SubmitButton>Adicionar Produto</SubmitButton>
