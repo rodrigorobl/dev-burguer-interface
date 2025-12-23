6ba8f6fdd871d8a0f9fc37666a4c463233bd180f
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2025-12-22 22:33:08 -0300

    finished project configuration

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3001/',
+	baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
-  const userData = localStorage.getItem('devburger:userData');
+	const userData = localStorage.getItem("devburger:userData");
 
-  const token = userData && JSON.parse(userData).token;
+	const token = userData && JSON.parse(userData).token;
 
-  config.headers.authorization = `Bearer ${token}`;
+	config.headers.authorization = `Bearer ${token}`;
 
-  return config;
+	return config;
 });
